585b5be06a84bebe0e3018ea0c34e75ea450996a
GalPogacnik777 <114558502+GalPogacnik777@users.noreply.github.com>
2023-01-05 07:59:14 +0100

    Add files via upload

---
+++ Objektivno programiranje/objektivno.py
@@ -0,0 +1,24 @@
+#imperativni programski jeziki-python
+#objektivno usmerjeni programski jeziki-java
+
+#terminal- python objektivno.py
+
+print("izpisi")
+
+def funkcija(x):
+    return x*3
+
+    #System.out.println("......")
+
+    #class Objektivno {
+    #public static void main()
+    #}
+
+    print(funkcija(7))
+
+    class Objektno():
+        def main():
+            print("izpisi")
+
+    objekt = Objektno()
+    print(objekt.main())
